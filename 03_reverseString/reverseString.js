a7b892c0fd98802a87abf1a0975cfd87567b32af
wiryawaneo <35023905+wiryawaneo@users.noreply.github.com>
2023-06-25 23:01:34 +0800

    Completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+   var split = string.split('');
+   var reversed = split.reverse();
+   var newString = reversed.join('');
+   return newString;
 };
 
 // Do not edit below this line
