62be3369c5659921495dba4768f1c21de25396f7
wiryawaneo <35023905+wiryawaneo@users.noreply.github.com>
2023-06-25 23:41:08 +0800

    Completed challenge 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
-
+const sumAll = function (firstNum, secondNum) {
+  if (
+    firstNum < secondNum &&
+    firstNum > 0 &&
+    secondNum > 0 &&
+    Number.isInteger(firstNum) &&
+    Number.isInteger(secondNum)
+  ) {
+    numDiff = secondNum - firstNum;
+    let newNum = firstNum;
+    for (i = 1; i <= numDiff; i++) {
+      newNum += firstNum + i;
+    }
+    return newNum;
+  } else if (
+    secondNum < firstNum &&
+    firstNum > 0 &&
+    secondNum > 0 &&
+    Number.isInteger(firstNum) &&
+    Number.isInteger(secondNum)
+  ) {
+    numDiff = firstNum - secondNum;
+    let newNum = secondNum;
+    for (i = 1; i < numDiff + 1; i++) {
+      newNum += secondNum + i;
+    }
+    return newNum;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
