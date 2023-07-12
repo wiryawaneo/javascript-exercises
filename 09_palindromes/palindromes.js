23dd8de0de1bca14096898c02637262518a73704
wiryawaneo <35023905+wiryawaneo@users.noreply.github.com>
2023-07-12 19:54:53 +0800

    Finished challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const newString = string.toLowerCase().replace(/[^a-z0-9]/g,"")
+return newString.split("").reverse().join("") == newString
 };
 
 // Do not edit below this line
