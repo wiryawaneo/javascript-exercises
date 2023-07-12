43b8ab2ba1bf3b5b723ad912d61c4505d8607263
wiryawaneo <35023905+wiryawaneo@users.noreply.github.com>
2023-07-12 19:47:51 +0800

    Finished calculator challenge

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, current) => sum+current,0)
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let product = 1;
+  for (i=0; i<args.length; i++) {
+    product *= args[i]
+  }
+  return product
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1;
+  product = 1;
+  for (let i = n; i > 0; i--) {
+   product *= i;
+  }
+  return product
 };
 
 // Do not edit below this line
